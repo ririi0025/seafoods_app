ae8ad4a16284592b9645f8cbf19d2a3452fd2544
ririi0025 <jerry.monton@normi.edu.ph>
2025-10-18 19:18:24 +0800

    Update product.dart

---
+++ lib/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id;
+  final String name;
+  final String vendor;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.vendor,
+    required this.price,
+  });
+}
